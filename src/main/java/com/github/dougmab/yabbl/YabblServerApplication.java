b6a90fcf32e169755d3f347b3e585e3930a5dff5
Douglas Brum <douglas.mateus2005@gmail.com>
2024-10-18 20:24:06 -0300

    initial commit

---
+++ src/main/java/com/github/dougmab/yabbl/YabblServerApplication.java
@@ -0,0 +1,13 @@
+package com.github.dougmab.yabbl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YabblServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YabblServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/dougmab/yabbl/YabblServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.dougmab.yabbl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YabblServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
