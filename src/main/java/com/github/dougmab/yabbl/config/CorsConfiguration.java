1690cca65bd9a4d5d05d1c6913b037b3bcd0da5e
Douglas Brum <douglas.mateus2005@gmail.com>
2024-10-25 09:19:57 -0300

    feat: add cors configuration

---
+++ src/main/java/com/github/dougmab/yabbl/config/CorsConfiguration.java
@@ -0,0 +1,15 @@
+package com.github.dougmab.yabbl.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(System.getenv("CLIENT_URL"))
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+    }
+}
